048b01d016fbe78875cb6f91f5ed6923b244ce68
Panagiotis Sakaridis <panosxsakar@gmail.com>
2020-10-11 17:17:28 +0300

    Updated deploy script

--- Scripts/gh-pages-deploy.js
+++ Scripts/gh-pages-deploy.js
@@ -1,9 +1,17 @@
 /* eslint-disable no-console */
 const execa = require("execa");
 const fs = require("fs");
+const rmrf = promisify(rimraf);
+
 (async () => {
+  let exitCode = 0;
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    const { stdout } = await execa("git", ["diff-index", "HEAD"]);
+    if (stdout) {
+        console.log("Please stash or commit changes first!");
+        process.exit(1);
+    }
     // eslint-disable-next-line no-console
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
@@ -15,13 +23,15 @@ const fs = require("fs");
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     console.log("Deleting dist");
-    await execa("del", [folderName]);
-    await execa("git", ["checkout", "-f", "master"]);
-    await execa("git", ["branch", "-D", "gh-pages"]);
-    console.log("Successfully deployed, check your settings");
+    await rmrf(folderName, { glob: false })
+    console.log("Successfully deployed");
   } catch (e) {
     // eslint-disable-next-line no-console
     console.log(e.message);
     process.exit(1);
+  } finally {
+    await execa("git", ["checkout", "-f", "master"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
   }
+process.exit(exitCode);
 })();
