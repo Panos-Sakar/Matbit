db58b97a84cd623f58f9cbb78e8e18f210898faf
Panagiotis Sakaridis <panosxsakar@gmail.com>
2020-10-28 04:31:59 +0200

    Update gh-pages-deploy-live-test.js

--- scripts/gh-pages-deploy-live-test.js
+++ scripts/gh-pages-deploy-live-test.js
@@ -6,15 +6,34 @@ const fs = require("fs");
   try {
     
     await execa("move", ["vue.config.js", "scripts\\tempFiles"]);
+    await execa("move", ["scripts\\scriptFiles\\vue.config.js", ".\\"]);
+
     console.log("Building...");
 
-    //await execa("npm", ["run", "build"]);
-    // Understand if it's dist or build folder
-    //const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await execa("npm", ["run", "build"]);
 
-    console.log("Successfully deployed");
+    await execa("move", ["vue.config.js", "scripts\\scriptFiles"]);
+    await execa("move", ["scripts\\tempFiles\\vue.config.js", ".\\"]);
+
+    console.log("Copying build output...");
+    
+    const folderName = fs.existsSync("dist") ? "dist\\" : "build\\"; // Understand if it's dist or build folder
+    await execa("Xcopy", [folderName, "..\\Matbit-live-test\\" , "/e/d/c/y"]);
+
+    console.log("Commiting ro Github...");
+    await execa("git", ["add", "--all"], { cwd: '../Matbit-live-test/' });
+    try{
+
+      await execa("git", ["commit", "-m", '"Update Matbit-live-test"'], { cwd: '../Matbit-live-test/' });
+    }catch (e) {
+      console.log(e.message);
+    }
+    await execa("git", ["push", "origin", "--all", "--force"], { cwd: '../Matbit-live-test/' });
+
   } catch (e) {
     console.log(e.message);
     process.exit(1);
+  }finally{
+    console.log("Successfully deployed");
   }
 })();
